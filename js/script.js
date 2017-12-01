0c5ea32426b0154942301255eb8d24b3a6431b1a
Jim Wrubel <jwrubel@gmail.com>
2017-12-01 10:30:29 -0500

    build the TTMDevs soundboard

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 	$("audio").removeAttr("controls").each(function(i, audioElement) {
 	    var audio = $(this);
 	    var that = this;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><img src="img/' + audio.attr("class") + '.png"/></a></li>').click(function() {that.play();}));
+	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><img src="img/' + audio.attr("class") + '.png"/></a><br /><div style="margin-left: 25%">"'+audio.attr("title")+'"</div></li>').click(function() {that.play();}));
 	});
 
 });
